31f0feefa3e079bf61f88e313346e0949e7c2ea0
Ott Julius (IFAG PSS RFS D2 SW ACE) <Julius.Ott@infineon.com>
2023-05-22 16:56:10 +0200

    latest train iris daatset

--- iris.py
+++ iris.py
@@ -1,6 +1,7 @@
 import numpy as np
 import seaborn as sns
 from matplotlib import pyplot as plt
+import matplotlib as mpl
 
 from sklearn.datasets import make_moons, make_circles, make_blobs, load_iris
 from sklearn.model_selection import train_test_split
@@ -13,17 +14,24 @@ from torch.utils.data import TensorDataset, DataLoader
 from benford_regularizer import compute_kl, quantile_loss
 from utils import progress_bar
 from utils import EarlyStopper
+from utils import mean_confidence_interval
 from sklearn.metrics import accuracy_score
 
 
-sns.set(style="darkgrid", font_scale=1.4)
+mpl.style.use("seaborn-deep")
+font = {'family' : 'normal',
+        'weight' : 'normal',
+        'size'   : 12}
+
+mpl.rc('font', **font)
 
 
 
-def main():
+def main(benford=False, seed=42):
+    torch.manual_seed(seed)
     X,y = load_iris(return_X_y=True)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.1, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=seed)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.3, random_state=seed)
     X_train_t = torch.from_numpy(X_train).to(torch.float32)
     y_train_t = torch.from_numpy(y_train).to(torch.float32)
     X_val_t = torch.from_numpy(X_val).to(torch.float32)
@@ -31,6 +39,7 @@ def main():
     X_test_t = torch.from_numpy(X_test).to(torch.float32)
     y_test_t = torch.from_numpy(y_test).to(torch.float32)
 
+    print("training ", len(X_train), "validation ", len(X_val), "testing ", len(X_test))
     train_dataset = TensorDataset(X_train_t, y_train_t)
     val_dataset = TensorDataset(X_val_t, y_val_t)
     test_dataset = TensorDataset(X_test_t, y_test_t)
@@ -40,17 +49,17 @@ def main():
 
     device = "cpu"
     net = nn.Sequential(
-            nn.Linear(4, 20),
+            nn.Linear(4, 10),
             nn.ReLU(),
-            nn.Linear(20, 3)
+            nn.Linear(10, 3)
             )
 
     net.to(device)
-    init_state_dict = net.state_dict()
     best_state_dict = net.state_dict()
 
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(net.parameters(), lr=0.05)
+    early_stopper = EarlyStopper(patience=10)
 
     def show_separation(save=False, name_to_save=""):
         sns.set(style="white")
@@ -80,16 +89,16 @@ def main():
         else:
             plt.show()
 
-    def train_bl(epoch, n_quantiles, scale=1):
+    def train_bl(epoch, scale=1):
         optimizer2 = optim.Adam(net.parameters(), lr=1e-3)
-        for _ in range(3):
+        for _ in range(10):
             net.train()
             optimizer2.zero_grad()
-            q_loss = quantile_loss(model=net, device=device, n_quantiles=n_quantiles) * scale
+            q_loss = quantile_loss(model=net, device=device) * scale
             q_loss.backward()
             optimizer2.step()
             bl_kl = compute_kl(net)
-            print('Train Epoch: {} mlh {:.5f} loss {:.5f}'.format(epoch, bl_kl, q_loss.item()))
+            #print('Train Epoch: {} mlh {:.5f} loss {:.5f}'.format(epoch, bl_kl, q_loss.item()))
         return bl_kl
 
     def train():
@@ -137,36 +146,79 @@ def main():
             _, predicted = outputs.max(1)
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
-        return correct/total, test_loss/total
+        
+        bl_kl = compute_kl(net)
+        return correct/total, test_loss/total, bl_kl
 
-    epochs = 15
-    best_acc = 0
-    for epoch in range(0,epochs):
-        train_acc, train_loss = train()
-        test_acc, test_loss = eval()
-        if test_acc > best_acc:
-            best_state_dict = net.state_dict()
-            best_acc = test_acc
-    test_acc, test_loss = test()
-    print(f"test acc {test_acc} test loss {test_loss}")
-
-    net.load_state_dict(init_state_dict)
-
-    best_acc = 0
+    best_acc = 110
+    epochs = 1000
+    best_epoch = 0
+    train_benford = False
+    bl_kls, val_accs, bl_epochs = [], [], []
     for epoch in range(0, epochs):
-        if epoch > 5 and epoch % 5 == 0:
-            _ = train_bl(epoch=epoch ,n_quantiles=10000, scale=0.1)
+        if train_benford:
+                _ = train_bl(epoch=epoch, scale=1)
+                bl_epochs.append(epoch)
+                print(f"benford {epoch}")
         else:
             train_acc, train_loss = train()
-        test_acc, test_loss = test()
-        if test_acc > best_acc:
+        val_acc, val_loss, bl_kl = eval()
+        val_accs.append(val_loss)
+        bl_kls.append(bl_kl)
+        if benford:
+            train_benford = early_stopper.early_stop(val_loss)
+        if val_loss < best_acc:
             best_state_dict = net.state_dict()
-            best_acc = test_acc
-        test_acc, test_loss = test()
-    print(f"test acc {test_acc} test loss {test_loss}")
+            best_acc = val_loss
+            best_epoch = epoch
+
+    net.load_state_dict(best_state_dict)
+    test_acc , test_loss = test()
+    print(f" best acc {best_acc} test acc {test_acc} in epoch {best_epoch}")
+    #print(f"test acc {test_acc} test loss {test_loss}")
+
+    return test_acc, np.asarray(val_accs), np.asarray(bl_kls), bl_epochs
 
 
     #show_separation(save=True, name_to_save="benford")
 
 if __name__ == "__main__":
-    main()
+    seeds = np.random.randint(0, int(1e6), size=(10,))
+
+    test_accs = []
+    seeds = [604859]
+    for seed in seeds:
+        print(seed)
+        test_acc, val_accs_bl, bl_kls_bl, benford_epochs  = main(benford=True, seed=seed)
+        test_accs.append(test_acc)
+        
+
+    mean, conf = mean_confidence_interval(test_accs)
+    print(f"BENFORD: mean {np.mean(test_accs)} std {np.std(test_accs)} 95% {conf} best {np.amax(test_accs)} worst {np.amin(test_accs)}")
+    test_accs = []
+    for seed in seeds:
+        test_acc, val_accs, bl_kls, _= main(benford=False, seed=seed)
+        test_accs.append(test_acc)
+
+    mean, conf = mean_confidence_interval(test_accs)
+    print(f" mean {np.mean(test_accs)} std {np.std(test_accs)} 95% {conf} best {np.amax(test_accs)} worst {np.amin(test_accs)}")
+
+    fig, (ax,ax1)  = plt.subplots(2,1, figsize=(6,4.5), sharex=True)
+    #fig.tight_layout()
+    ax.set_title("validation error")
+    ax.plot(val_accs_bl, label="MLP + BL reg")
+    #ax.scatter(np.argmin(val_accs_bl), val_accs_bl[np.argmin(val_accs_bl)], marker="^", label="min error BL reg", color="red", s=30)
+    #ax.scatter(np.argmin(val_accs), val_accs_bl[np.argmin(val_accs)], marker="^", label="min error", color="orange", s=30)
+    #ax1.scatter(benford_epochs, bl_kls_bl[benford_epochs])
+    ax.plot(val_accs, label="MLP")
+    ax1.set_title("BL KL")
+    ax1.plot(bl_kls_bl)
+    ax1.plot(bl_kls)
+    ax.set_yscale("log")
+    ax.set_ylim([0, 10**-2])
+    ax1.set_yscale("log")
+    ax.legend()
+    ax1.legend()
+    ax1.set_xlabel("epochs")
+    fig.savefig("2-layerMLP.pdf")
+    plt.show()
